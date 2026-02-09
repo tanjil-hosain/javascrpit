0d111615a99b6dad8d3d2be4763281e3ec19acec
tanjil-hosain <tanjilh85@gmail.com>
2026-02-09 12:35:48 +0600

    javasc

---
+++ script.js
@@ -0,0 +1,18 @@
+var a =2;
+var a =112;
+
+let b =" this is let"
+ c =" this is let 2"
+
+ const cup=" MILK"
+
+ let x =150;
+ let y = 50;
+ let sum = x+a;
+document.write(x,"<br>" ,c,"<br>");
+document.write( y,"<br>");
+console.log(sum)
+
+
+
+
