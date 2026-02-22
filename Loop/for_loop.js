1ee590316d116dd715eba1872095c5258091e961
tanjil-hosain <tanjilh85@gmail.com>
2026-02-22 11:02:47 +0600

    for_-loop

---
+++ Loop/for_loop.js
@@ -0,0 +1,19 @@
+ const prompt = require('prompt-sync')();
+ 
+for(i=0; i<5; i++){
+    console.log(i);
+}
+
+for(i=10; i>0; i--){
+    console.log(i);
+}
+
+let count_number= Number(prompt("Enter your count number"));
+for(let i = 0; i< count_number; i++){
+    console.log(i);
+}
+
+let student = ["Rakib ","Shakib","Faim","Layes","Polok","Tanjil"];
+for(i=0; i<student.length; i++){          // array er lenght dore element ber kora
+    console.log(student[i]);
+}
