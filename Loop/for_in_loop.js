ef3254974dbf56c98a333d9ecf32d8ee8a2beab6
tanjil-hosain <tanjilh85@gmail.com>
2026-02-22 11:31:53 +0600

    for in loop

---
+++ Loop/for_in_loop.js
@@ -0,0 +1,10 @@
+//for in loop
+
+let car = {
+    Name: "BMW",
+    Model: "M4",
+    color: "Black"
+}
+for(jhuri in car){                  //ekhane jhuri ta ami j kono variable declare korte parbo
+    console.log(jhuri+" :"+ car[jhuri]);
+}
