8323f0ae6600bd279b345d72942cb5d12e0b5ba4
tanjil-hosain <tanjilh85@gmail.com>
2026-02-23 10:43:23 +0600

    nasted-loop

---
+++ Loop/break&countinue.js
@@ -0,0 +1,8 @@
+
+
+for (let i=1; i<=20; i++){
+    if(i===10){
+        break;
+    }
+    console.log(i);
+}
---
+++ Loop/nasted-loop.js
@@ -0,0 +1,15 @@
+let data=[1,2,3,-1,-2,-3,0,0];
+let positive=0;
+let nagative=0;
+let zero=0;
+for(let i=0; i <data.length; i++){
+    if(data[i]>0){
+        positive++;
+    }else if(data[i]<0){
+        nagative++;
+    }else{
+        zero++;
+    }
+
+}
+console.log(positive+" "+nagative+" "+zero);
